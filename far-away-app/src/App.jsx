300c2a409770184abc828457007a8a1df0bcca77
bolaji87 <sakabolaji77@gmail.com>
2023-11-30 23:31:09 +0100

    added new components

--- far-away-app/src/App.jsx
+++ far-away-app/src/App.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "./components/Logo.jsx";
+import Form from "./components/Form.jsx";
+import PackingList from "./components/PackingList.jsx";
+import Stats from "./components/Stats.jsx";
+
+const initialItems = [
+	{ id: 1, description: "Passports", quantity: 2, packed: false },
+	{ id: 2, description: "Socks", quantity: 12, packed: false },
+	{ id: 3, description: "charger", quantity: 12, packed: true },
+];
 
 function App() {
+	const [items, setItems] = useState(initialItems);
+
+	function handleAddItem(newItem) {
+		setItems((items) => [...items, newItem]);
+	}
+
+	function handleDeleteItem(id) {
+		setItems((items) => items.filter((item) => item.id !== id));
+	}
 	return (
-		<div>
-			<h1>Appgit</h1>
+		<div className="app">
+			<Logo />
+			<Form onAddItem={handleAddItem} />
+			<PackingList items={items} onDeleteItem={handleDeleteItem} />
+			<Stats />
 		</div>
 	);
 }
--- far-away-app/src/components/PackingList.jsx
+++ far-away-app/src/components/PackingList.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
 	return (
 		<div className="list">
 			<ul>
 				{items.map((item) => (
-					<Items item={item} key={item.id} />
+					<Items item={item} key={item.id} onDeleteItem={onDeleteItem} />
 				))}
 			</ul>
 		</div>
 	);
 }
 
-function Items({ item }) {
+function Items({ item, onDeleteItem }) {
 	return (
 		<li>
 			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
 				{item.quantity} {item.description}
 			</span>
 
-			<button>❌</button>
+			<button onClick={() => onDeleteItem(item.id)}>❌</button>
 		</li>
 	);
 }
