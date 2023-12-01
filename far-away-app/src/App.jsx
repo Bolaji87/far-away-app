2b726351ef4bdde16a1cf39be86f7ecc02711db0
bolaji87 <sakabolaji77@gmail.com>
2023-12-01 01:03:49 +0100

    added changes to all

--- far-away-app/src/App.jsx
+++ far-away-app/src/App.jsx
@@ -7,7 +7,7 @@ import Stats from "./components/Stats.jsx";
 const initialItems = [
 	{ id: 1, description: "Passports", quantity: 2, packed: false },
 	{ id: 2, description: "Socks", quantity: 12, packed: false },
-	{ id: 3, description: "charger", quantity: 12, packed: true },
+	{ id: 3, description: "chargers", quantity: 12, packed: false },
 ];
 
 function App() {
@@ -20,12 +20,33 @@ function App() {
 	function handleDeleteItem(id) {
 		setItems((items) => items.filter((item) => item.id !== id));
 	}
+
+	function handleToggleItem(id) {
+		setItems((items) =>
+			items.map((item) =>
+				item.id === id ? { ...item, packed: !item.packed } : item
+			)
+		);
+	}
+
+	function handleClearList() {
+		const confirmed = window.confirm(
+			"Are you sure you want to delete all items?"
+		);
+		if (confirmed) setItems([]);
+	}
+
 	return (
 		<div className="app">
 			<Logo />
 			<Form onAddItem={handleAddItem} />
-			<PackingList items={items} onDeleteItem={handleDeleteItem} />
-			<Stats />
+			<PackingList
+				items={items}
+				onDeleteItem={handleDeleteItem}
+				OnToggleItem={handleToggleItem}
+				onClearList={handleClearList}
+			/>
+			<Stats items={items} />
 		</div>
 	);
 }
